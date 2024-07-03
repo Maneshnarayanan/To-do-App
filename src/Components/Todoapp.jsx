35c97e8d6bb726a73fcddeeb40e2e49d1b759003
Maneshnarayanan <maneshk27@gmail.com>
2024-07-04 00:59:41 +0530

    first commit

---
+++ src/Components/Todoapp.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import './Todoapp.css'
+export default class Todoapp extends Component {
+    state = {
+        input: "",
+        items: [],
+    };
+
+    handleChange = (event) => {
+        this.setState({
+            input: event.target.value,
+        });
+    };
+
+    storeItems = (event) => {
+        
+        event.preventDefault();
+        const { input } = this.state;
+
+        this.setState({
+            items: [...this.state.items, input],
+            input: ""
+        });
+    };
+
+    deleteItem = (key) => {
+        this.setState({
+            items: this.state.items.filter((data, index) => index !== key),
+        });
+    };
+
+    render() {
+        const { input, items } = this.state;
+        return (
+            <div className="todo-container">
+                <form className="input-section" onSubmit={this.storeItems}>
+                    <h1>Todo app</h1>
+                    <input type="text" value={input} placeholder="Enter items.." onChange={this.handleChange} />
+                </form>
+                <ul>
+                    {items.map((data, index) => (
+                        <li key={index}>
+                            {data} <i className="fa-solid fa-trash" onClick={()=> this.deleteItem(index)}></i>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
